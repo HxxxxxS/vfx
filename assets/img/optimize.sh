4060bde1ec7f98fe3ffe3392301000eed9f37294
HxxxxxS <16c52527@opayq.com>
2018-11-02 13:41:11 +0100

    Improved optimize.sh

--- assets/img/optimize.sh
+++
@@ -1,1 +0,0 @@
-gifsicle -v -b -O2 --colors=64 --use-col=web --resize-fit-width 500 *.gif
---
+++ assets/optimize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$(which gifsicle 2>/dev/null)" == '' ]; then
+    echo "This script requires https://www.lcdf.org/gifsicle, please download and try again."
+    exit 1
+fi
+
+i=$(ls -1 img/*.gif | wc -l)
+
+for f in *.gif; do
+    i=$(($i+1))
+    gifsicle -v -b -O2 --colors=64 --use-col=web --resize-fit-width 500 "$f"
+    mv "$f" "img/$i.gif"
+done
