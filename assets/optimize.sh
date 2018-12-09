b920c043490ff078f2129e4932d2cc6af877b982
HxxxxxS <16c52527@opayq.com>
2018-12-09 23:45:02 +0100

    Fixed optimize.sh

--- assets/optimize.sh
+++ assets/optimize.sh
@@ -5,12 +5,12 @@ if [ "$(which gifsicle 2>/dev/null)" == '' ]; then
     exit 1
 fi
 
-n=$(ls -1 img/*.gif | wc -l)
+i=$(ls -1 img/*.gif | wc -l)
 
 for f in *.gif; do
-    i=$(($n+1))
+    i=$(($i+1))
     gifsicle -v -b -O2 --colors=64 --use-col=web --resize-fit-width 500 "$f"
     mv "$f" "img/$i.gif"
 done
 
-echo "All gifs imported and optimized. Change gifcount to $n"
+echo "All gifs imported and optimized. Change gifcount to $i"
