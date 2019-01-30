78e5a8b4528954c0d6e2b7e7f41f230ac28e49db
HxxxxxS <16c52527@opayq.com>
2019-01-30 23:38:58 +0100

    Reindented Alea.js

--- assets/js/Alea.js
+++ assets/js/Alea.js
@@ -25,31 +25,31 @@ function Alea() {
       if (s1 < 0) {
         s1 += 1;
       }
-  s2 -= mash(args[i]);
+      s2 -= mash(args[i]);
       if (s2 < 0) {
         s2 += 1;
       }
     }
-mash = null;
+    mash = null;
 
-var random = function() {
-  var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
-  s0 = s1;
-  s1 = s2;
-  return s2 = t - (c = t | 0);
-};
-random.uint32 = function() {
-  return random() * 0x100000000; // 2^32
-};
-random.fract53 = function() {
-  return random() + 
-    (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
-};
-random.version = 'Alea 0.9';
-random.args = args;
-return random;
+    var random = function() {
+      var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
+      s0 = s1;
+      s1 = s2;
+      return s2 = t - (c = t | 0);
+    };
+    random.uint32 = function() {
+      return random() * 0x100000000; // 2^32
+    };
+    random.fract53 = function() {
+      return random() +
+      (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
+    };
+    random.version = 'Alea 0.9';
+    random.args = args;
+    return random;
 
-} (Array.prototype.slice.call(arguments)));
+  } (Array.prototype.slice.call(arguments)));
 };
 
 // From http://baagoe.com/en/RandomMusings/javascript/
@@ -62,17 +62,17 @@ function Mash() {
     data = data.toString();
     for (var i = 0; i < data.length; i++) {
       n += data.charCodeAt(i);
-  var h = 0.02519603282416938 * n;
-  n = h >>> 0;
-  h -= n;
-  h *= n;
-  n = h >>> 0;
-  h -= n;
-  n += h * 0x100000000; // 2^32
-}
-return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
-};
+      var h = 0.02519603282416938 * n;
+      n = h >>> 0;
+      h -= n;
+      h *= n;
+      n = h >>> 0;
+      h -= n;
+      n += h * 0x100000000; // 2^32
+    }
+    return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+  };
 
-mash.version = 'Mash 0.9';
-return mash;
+  mash.version = 'Mash 0.9';
+  return mash;
 }
