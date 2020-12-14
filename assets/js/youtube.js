ea94f0ab2a91614d7a2d09797bb573e7573015a3
Sophus Weidemann <sophusaugustweidemann@gmail.com>
2020-12-14 19:05:09 +0100

    Fix youtube api thing

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -62,7 +62,7 @@ function moreParameters()
 function onPlayerStateChange(event)
 {
     var target = event.target;
-    var id = target.f.id;
+    var id = target.h.id;
     var url = new URL(target.getVideoUrl());
     var videoId = url.searchParams.get('v');
 
